b26ed853bd798755f23eeb814bb893f2125f3470
SebRut <sebastian.rutofski@gmail.com>
2014-11-19 17:58:45 +0100

    refactoring

---
+++ d2mm/d2mm/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace de.sebastianrutofski.d2mm
+{
+    internal class Helpers
+    {
+        public static long GetDirectorySize(string dir)
+        {
+            long size = 0;
+
+            foreach (string file in Directory.GetFiles(dir))
+            {
+                size += new FileInfo(file).Length;
+            }
+
+            foreach (string subDir in Directory.GetDirectories(dir))
+            {
+                size += GetDirectorySize(subDir);
+            }
+
+            return size;
+        }
+    }
+}
